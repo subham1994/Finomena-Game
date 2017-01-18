3d655658e61267ab0e6b8baa1907e8483628aff3
subham <shubham.gaurav.1994@gmail.com>
2017-01-19 02:04:09 +0530

    init repo

---
+++ index.js
@@ -0,0 +1,171 @@
+const PORT = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const Timer = require('timer.js');
+const timer = new Timer();
+
+const colors = ['#2c3e50', '#e74c3c', '#8e44ad', '#2ecc71', '#ea4c88'];
+const users = {};
+const sockets = [];
+const bonusPointLocations = [];
+let gameConfigured = false;
+let playerNum = 1;
+let numTiles = 16;
+let currentlyPlaying = 0;
+let maxPlayers = 2;
+let gameDuration = 30;
+let pauseDuration = 3;
+let newUser;
+
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+io.on('connection', (socket) => {
+	sockets.push(socket);
+
+	if (gameConfigured) {
+		// paint the board on the screen with appropriate colors
+		socket.emit('draw grid', Object.assign({}, { users }, { numTiles }));
+	} else {
+		socket.emit('pre game');
+	}
+
+	// show the timer 
+	timer.on('tick', (ms) => {
+		io.emit('tick', { secondsLeft: Math.ceil(ms / 1000) });
+	});
+
+	// time to announce the winner
+	timer.on('end', () => {
+		let maxScore = -1;
+		let winner;
+		for (const socketId in users) {
+			if (users[socketId].score >= maxScore) {
+				winner = Object.assign({}, { name: users[socketId].name }, { socketId });
+				maxScore = users[socketId].score;
+			}
+		}
+		playerNum = 1;
+		io.emit('timeout', winner);
+		sockets.forEach((socket) => {
+			socket.disconnect();
+		});
+	});
+
+	// structure of user object
+	users[socket.id] = {
+		name: newUser,
+		color: colors.pop(),
+		playerNum: playerNum++,
+		tiles: [],
+		score: 0
+	}
+
+	// increment count if a user joins in
+	currentlyPlaying++;
+
+	// do all the setup work
+	socket.emit('join', users[socket.id]);
+	// build the sidebar listing current players
+	io.emit('user change', users);
+
+	socket.on('start game', function({ row, col, maxPlrs, pauseDur, gameDur }) {
+		gameConfigured = true;
+		const numTiles = parseInt(row, 10) * parseInt(col, 10);
+		maxPlayers = parseInt(maxPlrs, 10);
+		pauseDuration = parseInt(pauseDur, 10);
+		gameDuration = parseInt(gameDur, 10);
+		// get two random location to hide bonus points
+		const firstLocation = Math.ceil(Math.random() * numTiles);
+		let secondLocation;
+		do {
+			secondLocation = Math.ceil(Math.random() * numTiles);
+		} while (firstLocation === secondLocation);
+		bonusPointLocations.unshift(firstLocation, secondLocation);
+		io.emit('draw grid', Object.assign({}, { users }, { numTiles }));
+		timer.start(gameDuration);
+	});
+
+	// fired when user conquers a tile
+	socket.on('tile click', ({ tileId }) => {
+		const pauseTimer = new Timer();
+		const user = users[socket.id]
+		// check if the tile is a bonus tile
+		if (bonusPointLocations.indexOf(parseInt(tileId.split('-')[1], 10)) > -1) {
+			// notify the user
+			socket.emit('notification', {
+				text: 'Congrats, You earned 3 extra points !',
+				type: 'info'
+			});
+
+			// notify other users
+			socket.broadcast.emit('notification', {
+				text: `${user.name} just unlocked 3 extra points !`,
+				type: 'info'
+			});
+
+			// reward extra points along with usual points
+			user.score += 3;
+		}
+
+		// increment score by 1
+		user.score += 1;
+		// add it to user's tiles array to keep track of all the tiles
+		user.tiles.push(tileId);
+		// tell everyone somebody has occupied a tile
+		socket.broadcast.emit('tile change', { user, tileId });
+		// update the scores on the sidebar
+		io.emit('user change', users);
+
+		pauseTimer.on('tick', (ms) => {
+			io.emit('paused', { secondsLeft: Math.ceil(ms / 1000) });
+		});
+		pauseTimer.on('end', () => {
+			if (timer.getDuration()) {
+				timer.start();
+			}
+			io.emit('resume game');
+		});
+		timer.pause();
+		io.emit('pause game', { seconds: pauseDuration })
+		pauseTimer.start(pauseDuration);
+	});
+
+	socket.on('disconnect', () => {
+		// if a user disconnects, remove it from the users object
+		currentlyPlaying--;
+		const user = users[socket.id];
+		delete users[socket.id];
+		// take the color of the user and make it available again
+		colors.push(user.color);
+		// update the sidebar
+		io.emit('user change', users);
+		// unlock all its occupied tiles for others to occupy 
+		io.emit('unlock tiles', user);
+	});
+});
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/public/index.html');
+});
+
+app.post('/game', (req, res) => {
+	if (currentlyPlaying === maxPlayers) {
+		res.redirect('/');
+	}
+	newUser = req.body.username;
+	res.sendFile(__dirname + '/public/game.html');
+});
+
+app.get('/game', (req, res) => {
+	res.redirect('/');
+})
+
+http.listen(PORT, () => {
+	console.log('listening on port 3000');
+});
---
+++ public/js/app.js
@@ -0,0 +1,212 @@
+$(document).ready(function() {
+	
+	$('select').material_select();
+
+	$('.modal').modal({
+		dismissible: false, // Modal can be dismissed by clicking outside of the modal
+		opacity: 1, // Opacity of modal background
+    });
+
+	var socket = io();
+	var $playersList = $('#players-list');
+	var occupiedTiles = [];
+	var $counter = $('#counter');
+
+	function getTileColor(users, tileId) {
+		for (var socketId in users) {
+			var user = users[socketId]
+			if (user.tiles.indexOf(tileId) > -1) {
+				return user.color;
+			}
+		}
+		return undefined;
+	}
+
+	function showToast(text) {
+		var content = '<strong>' + text + '</strong>';
+		var $notification = $(content);
+		Materialize.toast($notification, 3000);
+	}
+
+	function showNotificationBanner(imgSrc, $text) {
+		var $notification = $('#notification');
+		$notification.css('display', 'inherit');
+		$notification.html('')
+		var $notificationCol = $('<div class="col l12 s12 m12"></div>');
+		var $notificationCard = $('<div class="card-panel center"></div>')
+		var $image = $('<img src="' + imgSrc + '"></img>');
+		$notificationCard.append($image);
+		$notificationCard.append($text);
+		$notification.append($notificationCol.append($notificationCard));
+	}
+
+	socket.on('notification', function(notif) {
+		showToast(notif.text);
+	});
+
+	socket.on('join', function(user) {
+		color = user.color;
+		username = user.name;
+
+		// show welcome message
+		showToast('Welcome to Finomenal Board game');
+
+
+		// ask the user to configure game if he is the first one to join
+		if (user.playerNum === 1) {
+			$('#configuration-modal').modal('open');
+
+			$('#configuration-form').on('submit', function(event) {
+				event.preventDefault();
+				socket.emit('start game', {
+					row: $('select[name=row]').val(),
+					col: 4,
+					maxPlrs: $('select[name=maxplrs]').val(),
+					pauseDur: $('select[name=pausedur]').val(),
+					gameDur: $('select[name=gamedur]').val()
+				});
+				$('#configuration-modal').modal('close');
+			});
+		}
+	});
+
+	socket.on('pre game', function() {
+		var $text = $('<h3 class="pause-text">Waiting for game configurations !</h3>');
+		showNotificationBanner('icons/hourglass.png', $text);
+	});
+
+	socket.on('draw grid', function(board) {
+		// create the grid with appropriate colors
+		$('#notification').css('display', 'none');
+		var users = board.users;
+		var numTiles = board.numTiles;
+		var $tiles = $('#tiles');
+		$tiles.html('');
+		for (var i = 1; i <= numTiles; i++) {
+			// check if the tile is already occupied and paint accordingly
+			var tileColor = getTileColor(users, 'tile-' + i) || '#FFF';
+			// build the occupied tile tracker
+			if (tileColor !== '#FFF') {
+				occupiedTiles.push('tile-' + i);
+			}
+			// build the grid
+			var $tileColumn = $('<div class="col l3 m4 s4"></div>');
+			var $tile = $('<p class="game-tile z-depth-1" id="tile-' + i + '"></p>');
+			$tile.css("background-color", tileColor);
+			$tileColumn.append($tile);
+			$tiles.append($tileColumn);
+		}
+	});
+
+	socket.on('tick', function(time) {
+		$counter.html('');
+		$counter.html(time.secondsLeft  + ' S');
+	});
+
+	socket.on('pause game', function(time) {
+		$('#tiles').css('display', 'none');
+		var $text = $('<h3 class="pause-text">Game will resume in \
+			<span style="color: #2196F3" id="remaining-time">' + time.seconds + '</span> second(s). \
+		</h3>');
+		showNotificationBanner('icons/stopwatch.png', $text);
+	});
+
+	socket.on('paused', function(time) {
+		var $remainingTimeNode = $('#remaining-time');
+		$remainingTimeNode.text('');
+		$remainingTimeNode.text(time.secondsLeft);
+	});
+
+	socket.on('resume game', function() {
+		$('#tiles').css('display', 'inherit');
+		$('#notification').css('display', 'none');
+	});
+
+	socket.on('timeout', function(winner) {
+		if (socket.id === winner.socketId) {
+			$('#winner-desc').append('Congratulations, <span style="color: #2196F3">You</span> won the game !');
+		} else {
+			$('#winner-desc').append('<span style="color: #2196F3">' + winner.name + '</span> won the game !');
+		}
+		$('#winner-modal').modal('open');
+	});
+
+	socket.on('user change', function(users) {
+		// initialize the counter
+		$counter.html('');
+		$counter.html('Timer');
+
+		// recreate the sidebar
+		$playersList.html('');
+		
+		var $header = $('\
+		<li class="collection-header">\
+			<h3 class="leaderboard-header center">Currently Playing</h3>\
+		</li>');
+		
+		$playersList.append($header);
+		
+		for (var socketId in users) {
+			let arrow = '';
+			if (users[socketId].name === username && users[socketId].color === color) {
+				arrow = '&#9658;';
+			}
+			var $list = $('\
+				<li class="collection-item">'
+					+ arrow + '<span class="new badge blue" data-badge-caption="tile(s)"><strong>' + users[socketId].score + '</strong></span>\
+					<div class="chip" style="color: #FFF; background-color: ' + users[socketId].color + '">\
+						<strong>' + users[socketId].name + '</strong>\
+					</div>\
+				</li>');
+			$playersList.append($list);
+		}
+	});
+
+	// update tile color if occupied by someone
+	socket.on('tile change', function(data) {
+		// keep track of occupied tiles
+		occupiedTiles.push(data.tileId);
+		showToast(data.user.name + ' conquered a tile !');
+		$('#' + data.tileId).css("background-color", data.user.color);
+	});
+
+	// tiles get unlocked if some user leaves
+	socket.on('unlock tiles', function(user) {
+		var tiles = user.tiles;
+		occupiedTiles = occupiedTiles.filter(function(tileId) {
+			var index = tiles.indexOf(tileId);
+			// make the disconnected user's tiles white again
+			if (index > -1) {
+				$('#' + tileId).css("background-color", "#FFF");
+			}
+			return index < 0;
+		});
+		showToast(user.name + ' left the game !');
+	});
+
+	$(document).on('mouseover', '.game-tile', function() {
+		if (occupiedTiles.indexOf($(this).attr('id')) === -1) {
+			$(this).css("background-color", color);
+		}
+	});
+
+	$(document).on('mouseout', '.game-tile', function() {
+		if (occupiedTiles.indexOf($(this).attr('id')) === -1) {
+			$(this).css("background-color", "#FFF");
+		}
+	});
+
+	$(document).on('click', '.game-tile', function() {
+		var id = $(this).attr('id');
+
+		// check if the tile is occupied already
+		if (occupiedTiles.indexOf(id) > -1) {
+			showToast('Tile already occupied !');
+		} else {
+			// keep track of occupied tiles
+			occupiedTiles.push(id);
+			$(this).css("background-color", color);
+			socket.emit('tile click', { tileId: id });
+		}
+	});
+});
